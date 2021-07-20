2cea95b2c4c65ee7d3949f880ffa54e0def31e4e
Falamir123 <falamir1992@gmail.com>
2021-07-20 15:26:56 +0700

    add lesson2

--- App.js
+++ App.js
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class ExampleClass extends React.Component {
-  render() {
-    return <p className="example__bordered">Example Class Component - {this.props.text} {this.props.text1}</p>
+const AUTHORS = {
+  Me: "Me: ",
+  BOT: "BOT: "
+}
+
+function Message(props) {
+
+  return <p>{props.author}{props.text}</p>
+}
+
+function App() {
+  const [messageList, setMessageList] = React.useState([]);
+  const [inputValue, setInputValue] = React.useState('');
+
+  React.useEffect(() => {
+    if (messageList.length &&
+      messageList[messageList.length - 1].author !== AUTHORS.BOT) {
+      setTimeout(() => {
+        setMessageList(currentMessageList => ([...currentMessageList, { author: AUTHORS.BOT, text: "Сообщение от бота" }]));
+      }, 1500);
+    }
+  }, [messageList]);
+
+  const messageChange = (e) => {
+    setInputValue(e.target.value)
   }
-}
+  const handleMessageSubmit = (e) => {
+    e.preventDefault();
+    setMessageList(currentMessageList => ([...currentMessageList, { author: AUTHORS.Me, text: inputValue }]));
+    setInputValue('');
+  }
 
-function Massage(props) {
-  console.log(props)
-
-  return <p>Example - {props.text} {props.text1}</p>
-}
-
-function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello {props.name}!
-        </p>
-        <Example text="Welcome!" text1="Передали пропсом текст" />
-        <ExampleClass text="Welcome!" text1="This is text 1" />
-      </header>
+      <div className="bordered">
+        {messageList.map((message, index) => (<Message author={message.author} key={index} text={message.text} />
+        ))}
+      </div>
+      <form className="app__form bordered" onSubmit={handleMessageSubmit}>
+        <input
+          required
+          className="bordered"
+          autoFocus
+          value={inputValue}
+          placeholder="Введите сообщение"
+          onChange={messageChange} />
+        <button>Отправить</button>
+      </form>
+
     </div>
   );
 }
